1fac5c47e56ccba10895bee1e9f14d52dfa9bcb7
mson90906250 <l90906250@gmail.com>
2019-02-18 02:05:22 +0800

    第五部分 製作動態的navigation

---
+++ core/init.php
@@ -0,0 +1,15 @@
+<!-- 用於跟database連結 -->
+<?php 
+	//mysqli_connect('localhost','user','password','dbname')
+	$db = mysqli_connect('localhost','root','','tutorial');
+
+	//判斷連線是否成功
+	//mysqli_connect_errno()如果連線過程中有錯誤時,會回傳true
+	if(mysqli_connect_errno()){
+		echo "Database connection failed following errors ".mysqli_connect_error();
+		//kill the page 即接在後面的内容無法呈現出來
+		die();
+	}
+
+
+ ?>
--- includes/navigation.php
+++ includes/navigation.php
@@ -1,17 +1,32 @@
+<?php 
+	$sql = "SELECT * FROM categories WHERE parent = 0";
+	//取得parent為0的資料 , 用於做navigation的主選項
+	$pquery = $db -> query($sql);//parentquery
+ ?>
+
 <!-- Top Nav Bar -->
 <nav class="navbar navbar-default navbar-fixed-top">
 	<div class="container">
 		<a href="index.php" class="navbar-brand">Shaunta's Boutique</a>
 		<ui class="nav navbar-nav">
-			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>
-				<ul class="dropdown-menu" role="menu">
-					<li><a href="#">Shirts</a></li>
-					<li><a href="#">Pants</a></li>
-					<li><a href="#">Shoes</a></li>
-					<li><a href="#">Accessories</a></li>
-				</ul>
-			</li>
+			<!-- 製作loop -->
+			<?php while($parent = $pquery -> fetch_assoc()) : ?>
+				<?php 
+					$parent_id = $parent['id']; 
+					$sql2 = "SELECT * FROM categories WHERE parent = '$parent_id'";
+					$cquery = $db -> query($sql2);//childquery
+				?>
+				<!-- Menu Items -->
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $parent['category']; ?><span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<?php while($child = $cquery -> fetch_assoc()) : ?>
+							<li><a href="#"><?php echo $child['category'] ?></a></li>
+						<?php endwhile; ?>
+					</ul>
+				</li>
+			
+			<?php endwhile; ?>
 		</ui>
 	</div>
 </nav>
