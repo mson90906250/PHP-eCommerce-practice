7044469cefad4e8975844138742927b00a19e20f
mson90906250 <l90906250@gmail.com>
2019-02-18 22:14:49 +0800

    第六部分 改成動態生成feature商品

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
 	include "includes/headerfull.php";
 	include "includes/leftbar.php";
 
+	$sql = "SELECT * FROM product WHERE featured = 1 ";
+	//來自init.php的$db
+	$featured = $db->query($sql);
+
 ?>
 
 
@@ -12,86 +16,18 @@
 		<div class="col-md-8">
 			<div class="row">
 				<h2 class="text-center">Feature Products</h2>
-				<div class="col-md-3">
-					<h4>Levis Jeans</h4>
-					<img src="images/products/men4.png" alt="Levis Jeans" class="img-thumb">
-					<p class="list-price text-danger">List Price: <s>$54.99</s></p>
-					<p class="price">Our Price: $39.99</p>		
-					<button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">
-						Details
-					</button>
-				</div>
-
-				<div class="col-md-3">
-					<h4>Woman's Shirt</h4>
-					<img src="images/products/women7.png" alt="Woman's Shirt" class="img-thumb">
-					<p class="list-price text-danger">List Price: <s>$74.99</s></p>
-					<p class="price">Our Price: $49.99</p>		
-					<button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">
-						Details
-					</button>
-				</div>
-
-				<div class="col-md-3">
-					<h4>Hollister Shirt</h4>
-					<img src="images/products/men1.png" alt="Hollister Shirt" class="img-thumb">
-					<p class="list-price text-danger">List Price: <s>$24.99</s></p>
-					<p class="price">Our Price: $19.99</p>		
-					<button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">
-						Details
-					</button>
-				</div>
-
-				<div class="col-md-3">
-					<h4>Fancy Shoes</h4>
-					<img src="images/products/women6.png" alt="Fancy Shoes" class="img-thumb ">
-					<p class="list-price text-danger">List Price: <s>$64.99</s></p>
-					<p class="price">Our Price: $49.99</p>		
-					<button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">
-						Details
-					</button>
-				</div>
-
-				<div class="col-md-3">
-					<h4>Boys Hoodie</h4>
-					<img src="images/products/boys1.png" alt="Boys Hoodie" class="img-thumb">
-					<p class="list-price text-danger">List Price: <s>$24.99</s></p>
-					<p class="price">Our Price: $18.99</p>		
-					<button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">
-						Details
-					</button>
-				</div>
-
-				<div class="col-md-3">
-					<h4>Girls Dress</h4>
-					<img src="images/products/girls3.png" alt="Girls Dress" class="img-thumb">
-					<p class="list-price text-danger">List Price: <s>$34.99</s></p>
-					<p class="price">Our Price: $19.99</p>		
-					<button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">
-						Details
-					</button>
-				</div>
-
-
-				<div class="col-md-3">
-					<h4>Woman's Skirt</h4>
-					<img src="images/products/women3.png" alt="Woman's Skirt" class="img-thumb">
-					<p class="list-price text-danger">List Price: <s>$84.99</s></p>
-					<p class="price">Our Price: $69.99</p>		
-					<button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">
-						Details
-					</button>
-				</div>
-
-				<div class="col-md-3">
-					<h4>Purse</h4>
-					<img src="images/products/women5.png" alt="Purse" class="img-thumb">
-					<p class="list-price text-danger">List Price: <s>$544.99</s></p>
-					<p class="price">Our Price: $399.99</p>		
-					<button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">
-						Details
-					</button>
-				</div>
+				<?php while($product = $featured->fetch_assoc()) : ?>
+					<div class="col-md-3">
+						<!-- <?= a ?> 等同 <?php echo a ?> -->
+						<h4><?= $product['title'] ?></h4>
+						<img src="<?= $product['image'] ?>" alt="<?= $product['title'] ?>" class="img-thumb">
+						<p class="list-price text-danger">List Price: <s>$<?= $product['list_price'] ?></s></p>
+						<p class="price">Our Price: $<?= $product['price'] ?></p>		
+						<button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#details-1">
+							Details
+						</button>
+					</div>
+				<?php endwhile; ?>
 			</div>
 		</div>
 
